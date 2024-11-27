2d71cd5310bbd2ea2d244a63bb56518525c0b209
merodriguezti <merodriguezti@unadvirtual.edu.co>
2024-11-26 21:58:04 -0500

    maquetacion del proyecto

---
+++ main.py
@@ -0,0 +1,39 @@
+import datatime
+
+#Lista para almacenar los experimentos
+listaDeExperimentos=[ "Experimento 1", "26/11/2024", "fisica", [5,6,7,8,9]]
+
+#Funcion para agregar un nuevo experimento
+def agregarExperimento():
+    pass
+
+
+#Funcion para eliminar un experimento
+def eliminarExperimento():
+    pass
+
+
+#Funcion para visualizar mis experimentos
+def visualizarExperimentos():
+    pass
+
+#funcion para calcular las estadisticas
+def calcularEstadisticas():
+    pass 
+
+#Funcion para comparar eexperimentos
+def compararExperimentos():
+     pass
+
+#funcion para generar un informe
+def generarInforme():
+    pass
+
+#Funcion para crear un menu
+def menu():
+    pass
+
+#funcion Principal
+def main():
+    pass
+
